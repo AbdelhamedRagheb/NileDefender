4457f0f08bf4a4cefbb3d15d010a6f8770314e04
AbdelhamedRagheb <x2912003x@gmail.com>
2025-11-25 09:47:29 +0200

    Update App.py

--- App.py
+++ App.py
@@ -5,8 +5,46 @@ app = Flask(__name__)
 
 # Route for root URL
 @app.route('/')
+def root():
+    return 'root page'
+
+# Route for home URL
+@app.route('/home')
 def home():
-    return 'Hello World'
+    return 'Home page'
+
+# Route for about URL
+@app.route('/about')
+def about():
+    return 'About page'
+
+# Route for Results URL
+@app.route('/results')
+def results():
+    return 'Results page'
+
+# Route for login URL
+@app.route('/login')
+def login():
+    return 'Login page'
+
+# Route for signup URL
+@app.route('/signup')
+def signup():
+    return 'Signup page'
+
+# Route for dashboard URL
+@app.route('/dashboard')
+def dashboard():
+    return 'Dashboard page'
+
+# Route for profile URL
+@app.route('/profile')
+def profile():
+    return 'Profile page'
+
+
+
 
 
 if __name__ == '__main__':
